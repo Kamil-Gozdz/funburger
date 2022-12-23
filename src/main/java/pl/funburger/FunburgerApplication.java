52c101c77bfd58d879b9de54fffb95c7976a4cf0
Kamil <kamilgozdz23@gmail.com>
2022-12-23 12:03:26 +0100

    init commit

---
+++ src/main/java/pl/funburger/FunburgerApplication.java
@@ -0,0 +1,13 @@
+package pl.funburger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunburgerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunburgerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/funburger/FunburgerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.funburger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunburgerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
