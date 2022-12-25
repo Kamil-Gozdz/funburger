7532af1cbae3fef1e3a7cf88cf21956d103c57c5
Kamil <kamilgozdz23@gmail.com>
2022-12-25 16:33:54 +0100

    feature 6/ create and delete methods

--- src/main/java/pl/funburger/service/recipe/RecipeService.java
+++ src/main/java/pl/funburger/service/recipe/RecipeService.java
@@ -1,4 +1,10 @@
 package pl.funburger.service.recipe;
 
+import pl.funburger.model.dto.RecipeDto;
+
 public interface RecipeService {
+
+    RecipeDto createRecipe(RecipeDto recipeDto);
+
+    void deleteRecipe(RecipeDto recipeDto);
 }
--- src/main/java/pl/funburger/service/user/UserServiceImpl.java
+++ src/main/java/pl/funburger/service/user/UserServiceImpl.java
@@ -1,5 +1,27 @@
 package pl.funburger.service.user;
 
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.funburger.model.dto.UserDto;
+import pl.funburger.model.mapper.UserMapper;
+import pl.funburger.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
 
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    @Override
+    @Transactional
+    public UserDto createUser(UserDto userDto) {
+        return userMapper.toDto(userRepository.save(userMapper.toNewEntity(userDto)));
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(UserDto userDto) {
+        userRepository.delete(userMapper.toNewEntity(userDto));
+    }
 }
