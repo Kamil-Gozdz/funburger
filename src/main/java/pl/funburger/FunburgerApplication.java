1a2256154b17e60b7aac97c6cdb01dbd8130a1c0
Kamil <kamilgozdz23@gmail.com>
2023-01-02 16:36:30 +0100

    feature 9/ adding getAllUsers method

--- src/main/java/pl/funburger/FunburgerApplication.java
+++ src/main/java/pl/funburger/FunburgerApplication.java
@@ -2,7 +2,6 @@ package pl.funburger;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 
 @SpringBootApplication
--- src/main/java/pl/funburger/service/user/UserService.java
+++ src/main/java/pl/funburger/service/user/UserService.java
@@ -3,6 +3,8 @@ package pl.funburger.service.user;
 import pl.funburger.model.dto.UserCreateDto;
 import pl.funburger.model.dto.UserDto;
 
+import java.util.List;
+
 public interface UserService {
 
     UserDto createUser(UserCreateDto createDto);
@@ -10,4 +12,6 @@ public interface UserService {
     void deleteUserById(Long id);
 
     UserDto getLoggedInUser();
+
+    List<UserDto> getAllUsers();
 }
